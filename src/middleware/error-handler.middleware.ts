faa41e35470bb6553341e325748d5798c432bfbb
Erez Gershon <Erez@appnxt.net>
2021-08-16 15:30:37 +0300

    error middleware with res status

--- src/middleware/error-handler.middleware.ts
+++ src/middleware/error-handler.middleware.ts
@@ -2,7 +2,9 @@ import {Request, Response} from 'express';
 
 import {GenericError} from '../interfaces/generic-error.interface';
 
-export function handleErrors(err: GenericError, _: Request, res: Response) {
+// export function handleErrors(err: GenericError, _: Request, res: Response) {
+export function handleErrors(err: GenericError, _: Request, res: Response, next: any) {
+  console.log(res)
   const {status = 500, message} = err;
   return res.status(status).send(message);
 }
